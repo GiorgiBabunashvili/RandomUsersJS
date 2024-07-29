220de8fd4ee15417c98924780db3f77d6af40c18
shanME <giorgie103188@gmail.com>
2024-07-29 12:28:18 +0400

    fetch new user and add data array

--- script.js
+++ script.js
@@ -5,4 +5,23 @@ const showMilionaiers = document.getElementById("show-milionaiers");
 const sortRichest = document.getElementById("sort-richest");
 const calculateWealth = document.getElementById("calculate-wealth");
 
-console.log(addUser);
+let data = [];
+
+//Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api/");
+  const data = await res.json();
+
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+}
+
+//Add new obj to data array
+function addData(obj) {
+  data.push(obj);
+}

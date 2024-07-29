78390a8989a3919562521b02295b26d5872aa2d6
shanME <giorgie103188@gmail.com>
2024-07-29 12:08:34 +0400

    declear variable

---
+++ script.js
@@ -0,0 +1,8 @@
+const main = document.getElementById("main");
+const addUser = document.getElementById("add-user");
+const doubleMoney = document.getElementById("double-money");
+const showMilionaiers = document.getElementById("show-milionaiers");
+const sortRichest = document.getElementById("sort-richest");
+const calculateWealth = document.getElementById("calculate-wealth");
+
+console.log(addUser);

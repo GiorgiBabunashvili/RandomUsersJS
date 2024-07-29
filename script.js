c44fe25507053739e0f5494e1f0aaa521c22d33b
shanME <giorgie103188@gmail.com>
2024-07-29 12:54:23 +0400

    add new user

--- script.js
+++ script.js
@@ -24,11 +24,27 @@ async function getRandomUser() {
 //Add new obj to data array
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
 
 //Update DOM
 function updateDOM(provideData = data) {
   main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+  provideData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
 }
 
-addUser.addEventListener("click", () => {});
+//Format money
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+//event listeners
+addUser.addEventListener("click", getRandomUser);
